caeef9065bc6d8cc0133a1b6d759a8a94710debd
Walisson Rodrigues <contact@walisson.com>
2020-12-22 23:39:52 +1100

    Header component UI implementation + Importing MaterialUI to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from './components/Header/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Facebook Clone</h1>
+      <Header />
     </div>
   );
 }
