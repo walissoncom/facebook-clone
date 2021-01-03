1451c0b826888b01b3197997f93e71c689056540
Walisson Rodrigues <contact@walisson.com>
2021-01-03 21:53:06 +1100

    Page body container size adjustment

--- src/components/StoryReel/StoryReel.js
+++ src/components/StoryReel/StoryReel.js
@@ -21,7 +21,7 @@ function StoryReel() {
             <Story
                 image="https://scontent.fsyd8-1.fna.fbcdn.net/v/t1.0-9/133529670_3724194967658317_4628561641904669632_o.jpg?_nc_cat=109&ccb=2&_nc_sid=5b7eaf&_nc_ohc=pQqmWVrZh1UAX_quUYD&_nc_ht=scontent.fsyd8-1.fna&oh=6751db4ad754fbffa65bb73de173988e&oe=60164547"
                 profileSrc="https://scontent.fsyd8-1.fna.fbcdn.net/v/t1.0-1/p120x120/10430899_743999102287384_546402217750594119_n.png?_nc_cat=105&ccb=2&_nc_sid=1eb0c7&_nc_ohc=hfml-djZ-C0AX_3m0sO&_nc_ht=scontent.fsyd8-1.fna&_nc_tp=30&oh=d96988fc438b4000da29d63281aedfa2&oe=60169F6C"
-                title="Programmers Life" />
+                title="Dev's Life" />
 
             <Story
                 image="https://scontent.fsyd8-1.fna.fbcdn.net/v/t1.0-9/s1080x2048/133682614_3675134745899446_494825799030996038_o.jpg?_nc_cat=105&ccb=2&_nc_sid=5b7eaf&_nc_ohc=xH-P_ncgsYEAX_Lr2Iq&_nc_ht=scontent.fsyd8-1.fna&tp=7&oh=914c824c131c2fda8f880cdede60d30a&oe=601525DE"
@@ -32,6 +32,11 @@ function StoryReel() {
                 image="https://scontent.fsyd8-1.fna.fbcdn.net/v/t1.0-9/s1080x2048/133549375_3675208349225419_802263445019206684_o.jpg?_nc_cat=104&ccb=2&_nc_sid=5b7eaf&_nc_ohc=akzB_Dk0F-EAX_z__XP&_nc_ht=scontent.fsyd8-1.fna&tp=7&oh=ea2157b67cb69413caaae0fbd55d15b2&oe=601763AD"
                 profileSrc="https://scontent.fsyd8-1.fna.fbcdn.net/v/t1.0-1/cp0/p60x60/51329002_2139113679501568_6563218904603164672_o.jpg?_nc_cat=1&ccb=2&_nc_sid=1eb0c7&_nc_ohc=7dB8qRY1fvwAX_93QZd&_nc_ht=scontent.fsyd8-1.fna&tp=27&oh=1dda1fd7976cc842b59e71d25af439b9&oe=60176BB7"
                 title="Valuable Generation" />
+
+            <Story
+                image="https://scontent.fsyd8-1.fna.fbcdn.net/v/t1.0-9/134361344_3726950840716063_3473422409193183754_o.jpg?_nc_cat=106&ccb=2&_nc_sid=5b7eaf&_nc_ohc=jAxtRA1ozgcAX99W-_u&_nc_ht=scontent.fsyd8-1.fna&oh=8a03a902a7d877e88e319b1366476a04&oe=60165985"
+                profileSrc="https://scontent.fsyd8-1.fna.fbcdn.net/v/t1.0-1/cp0/p40x40/23473074_10155031875776961_8482140412038626648_n.jpg?_nc_cat=1&ccb=2&_nc_sid=1eb0c7&_nc_ohc=mpijnlaqxFUAX_GodHr&_nc_ht=scontent.fsyd8-1.fna&tp=27&oh=b9a5270ad0033bddcc1c7f74fb63f3b5&oe=6015D20A"
+                title="Bill Gates" />
 
         </div>
     )
